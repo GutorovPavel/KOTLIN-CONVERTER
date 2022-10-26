16d1232aed6e80135da043e141554b3af8e04fb5
GutorovPavel <pavelgutorovgut@gmail.com>
2022-10-27 00:13:01 +0300

    final product

---
+++ app/src/main/java/com/example/converter_lab/Currency.kt
@@ -0,0 +1,30 @@
+package com.example.converter_lab
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import kotlinx.android.synthetic.main.fragment_currency.*
+
+class Currency : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_currency, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = Currency()
+    }
+}
--- app/src/main/java/com/example/converter_lab/MainActivity.kt
+++ app/src/main/java/com/example/converter_lab/MainActivity.kt
@@ -1,43 +1,212 @@
 package com.example.converter_lab
 
+import android.content.ClipData
+import android.content.ClipboardManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
+import android.text.InputType
+import android.text.SpannableStringBuilder
 import android.text.TextWatcher
 import android.util.Log
+import android.view.ActionMode
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.Toast
+import android.widget.*
+import androidx.fragment.app.Fragment
+import com.example.converter_lab.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import org.json.JSONObject
-import java.net.URL
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    var baseCurrency = "BYN"
-    var convertedToCurrency = "EUR"
-    var conversionRate = 0f
+    lateinit var binding: ActivityMainBinding
 
-    var convertedToDistance = "inch"
-    var convertedToWeight = "pounds"
+    private var inputReady: String = ""
 
+    private var convertFrom: String = ""
+    private var convertTo: String = ""
+
+    private lateinit var clipboard: ClipboardManager
+
+    private val convertibleValues: Map<String, Double> = mapOf(
+        "BYN" to 1.0,  "EUR" to 2.45996, "USD" to 2.53473,
+        "m" to 1.0, "dm" to 0.1, "cm" to 0.01,
+        "kg" to 1.0, "g" to 0.001, "t" to 1000.0
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        spinnerSetup()
-        textChanged()
+        replaceFragment(Currency())
+        spinnerSetup(R.array.currencies)
+        cTitle.text = "CURRENCY CONVERTER"
+        clipboard = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
+
+        binding.bottomAppBar.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.currency_fr -> {
+                    replaceFragment(Currency.newInstance())
+                    spinnerSetup(R.array.currencies)
+                    cTitle.text = "CURRENCY CONVERTER"
+                }
+                R.id.distance_fr -> {
+                    replaceFragment(Distance.newInstance())
+                    spinnerSetup(R.array.distance)
+                    cTitle.text = "DISTANCE CONVERTER"
+                }
+                R.id.weight_fr -> {
+                    replaceFragment(Weight.newInstance())
+                    spinnerSetup(R.array.weight)
+                    cTitle.text = "WEIGHT CONVERTER"
+                }
+
+                else -> {}
+            }
+            true
+        }
+
+        forbidActions(inputArea)
+        textChange()
     }
 
-    private fun textChanged() {
-        et_firstConversion.addTextChangedListener(object: TextWatcher {
+    private fun forbidActions(input: TextView){
+        input.customSelectionActionModeCallback = object : ActionMode.Callback{
+            override fun onCreateActionMode(p0: ActionMode?, p1: Menu?): Boolean {
+                return false
+            }
+
+            override fun onPrepareActionMode(p0: ActionMode?, p1: Menu?): Boolean {
+                return false
+            }
+
+            override fun onActionItemClicked(p0: ActionMode?, p1: MenuItem?): Boolean {
+                return true
+            }
+
+            override fun onDestroyActionMode(p0: ActionMode?) {}
+        }
+    }
+
+    private fun reload() {
+        inputArea.setText("")
+        outputArea.text = ""
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        //inputArea.showSoftInputOnFocus = false
+        inputArea.inputType = InputType.TYPE_NULL
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.place_holder, fragment).commit()
+        reload()
+    }
+
+    /////////////////////// BIND BUTTONS ////////////////////////
+
+    private fun updateText(str: String) {
+        inputArea.showSoftInputOnFocus = false
+        inputArea.inputType = InputType.TYPE_CLASS_TEXT
+
+        var max = 16
+        val oldStr: String = inputArea.text.toString()
+        var cursorPos = inputArea.selectionStart
+        var leftStr: String = oldStr.substring(0, cursorPos)
+        val rightStr: String = oldStr.substring(cursorPos)
+
+        if (oldStr.endsWith(".")) max += 1
+
+        if (inputArea.text.length < max) {
+            if (oldStr == "0" && str != ".") {
+                leftStr = ""
+                inputArea.setText(String.format("%s%s%s", leftStr, str, rightStr))
+                cursorPos = 0
+            }
+            if ("." in oldStr && str == ".") {
+            } else {
+                inputArea.setText(String.format("%s%s%s", leftStr, str, rightStr))
+                inputArea.setSelection(cursorPos + 1)
+            }
+        }
+    }
+
+    fun clearBtn(view: View) {
+        inputArea.setText("")
+    }
+
+    fun delBtn(view: View) {
+        val cursorPos = inputArea.selectionStart
+        val textLen = inputArea.text.length
+
+        if (cursorPos != 0 && textLen != 0) {
+            val selection: SpannableStringBuilder = (SpannableStringBuilder(inputArea.text))
+            selection.replace(cursorPos - 1, cursorPos, "")
+            inputArea.text = selection
+            inputArea.setSelection(cursorPos - 1)
+        }
+    }
+
+    fun numberBtn(view: View) {
+        if (view is androidx.appcompat.widget.AppCompatButton) {
+            updateText(view.text.toString())
+        }
+    }
+
+    fun swapBtn(view: View) {
+        val temp = spinnerFrom.selectedItemId.toInt()
+        spinnerFrom.setSelection(spinnerTo.selectedItemId.toInt())
+        spinnerTo.setSelection(temp)
+        conversion()
+    }
+
+    fun copyBtn(view: View) {
+        val clip = ClipData.newPlainText("TextView", outputArea.text.toString())
+        clipboard.setPrimaryClip(clip)
+        Toast.makeText(this, "Copied!", Toast.LENGTH_SHORT).show()
+    }
+
+    fun pasteBtn(view: View) {
+        val copied = clipboard.primaryClip?.getItemAt(0)
+        updateText(copied?.text.toString())
+        Toast.makeText(this, "Pasted!", Toast.LENGTH_SHORT).show()
+    }
+
+    ////////////////////////////////////////////////////////////
+
+    private fun conversion() {
+        if (inputArea != null &&
+            inputArea.text.isNotEmpty() &&
+            inputArea.text.isNotBlank())
+        {
+            Formatter.BigDecimalLayoutForm.SCIENTIFIC
+            val res = (inputArea.text.toString().toBigDecimal()) *
+                    (convertibleValues[convertFrom]!! / convertibleValues[convertTo]!!)
+                        .toBigDecimal()
+
+            inputReady = res.toPlainString()
+            if (inputReady.length > 16) {
+                var dotIndex = inputReady.indexOf(".")
+
+                if (dotIndex == 15) {
+                    dotIndex += 2
+                    inputReady = inputReady.substring(0, dotIndex)
+                }
+                else {
+                    inputReady = inputReady.substring(0, 16)
+                }
+            }
+            outputArea.text = inputReady
+        }
+        else {
+            outputArea.text = ""
+        }
+    }
+
+    private fun textChange() {
+        inputArea.addTextChangedListener(object: TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 Log.d("Main", "before text changed")
             }
@@ -46,180 +215,41 @@ class MainActivity : AppCompatActivity() {
                 Log.d("Main", "on text changed")
             }
 
-            override fun afterTextChanged(s: Editable?) {
-                try {
-                    getApiResult()
-                    getDistanceResult()
-                    getWeightResult()
-                }
-                catch (e: Exception) {
-                    Log.e("Main", "$e")
-                }
+            override fun afterTextChanged(p0: Editable?) {
+                conversion()
             }
-
         })
     }
 
-    private fun getApiResult() {
-        if(et_firstConversion != null &&
-            et_firstConversion.text.isNotEmpty() &&
-            et_firstConversion.text.isNotBlank()) {
+    private fun spinnerSetup(arr: Int) {
+        val arrayAdapter =
+            ArrayAdapter.createFromResource(this, arr, android.R.layout.simple_spinner_item)
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
-            //apikey=FcbkosMOF7884DLJbPjAHmgcuJ8PAMEx&  // API-key
-            //val API = "https://api.apilayer.com/fixer/latest?base={$baseCurrency}&symbols={$convertedToCurrency}&apikey=FcbkosMOF7884DLJbPjAHmgcuJ8PAMEx"
-            val API = "*"
+        val spinner1: Spinner = findViewById(R.id.spinnerFrom)
+        val spinner2: Spinner = findViewById(R.id.spinnerTo)
+        spinner1.adapter = arrayAdapter
+        spinner2.adapter = arrayAdapter
 
-            if(baseCurrency == convertedToCurrency) {
-                Toast.makeText(applicationContext,
-                    "Cannot convert the same currency", Toast.LENGTH_SHORT).show()
-            }
-            else {
-                GlobalScope.launch(Dispatchers.IO) {
-                    try {
-                        val apiResult = URL(API).readText()
-                        val jsonObject = JSONObject(apiResult)
-
-                        conversionRate = jsonObject.getJSONObject("rates").getString(convertedToCurrency).toFloat()
-
-                        withContext(Dispatchers.Main) {
-                            val text = (et_firstConversion.text.toString().toFloat() * conversionRate).toString()
-                            et_secondConversion?.setText(text)
-                        }
-                    }
-                    catch (e: Exception) {
-                        Log.e("Main", "$e")
-                    }
-                }
-            }
-        }
-    }
-
-    private fun getDistanceResult() {
-        if(et2_firstConversion != null &&
-            et2_firstConversion.text.isNotEmpty() &&
-            et2_firstConversion.text.isNotBlank()) {
-
-            if(convertedToDistance == "inch") {
-                val text = (et2_firstConversion.text.toString().toFloat() * 39.37).toString()
-                et2_secondConversion?.setText(text)
-            }
-            if(convertedToDistance == "cm") {
-                val text = (et2_firstConversion.text.toString().toFloat() * 100).toString()
-                et2_secondConversion?.setText(text)
-            }
-            if(convertedToDistance == "km") {
-                val text = (et2_firstConversion.text.toString().toFloat() * 0.01).toString()
-                et2_secondConversion?.setText(text)
-            }
-        }
-    }
-
-    private fun getWeightResult() {
-        if(et3_firstConversion != null &&
-            et3_firstConversion.text.isNotEmpty() &&
-            et3_firstConversion.text.isNotBlank()) {
-
-            if(convertedToDistance == "pounds") {
-                val text = (et2_firstConversion.text.toString().toFloat() * 0.061).toString()
-                et3_secondConversion?.setText(text)
-            }
-            if(convertedToDistance == "g") {
-                val text = (et2_firstConversion.text.toString().toFloat() * 1000).toString()
-                et3_secondConversion?.setText(text)
-            }
-            if(convertedToDistance == "centner") {
-                val text = (et2_firstConversion.text.toString().toFloat() * 0.01).toString()
-                et3_secondConversion?.setText(text)
-            }
-        }
-    }
-
-    private fun spinnerSetup() {
-        val spinner1: Spinner = findViewById(R.id.spinner_firstConversion)
-        val spinner2: Spinner = findViewById(R.id.spinner_secondConversion)
-        val spinner3: Spinner = findViewById(R.id.spinner2_secondConversion)
-        val spinner4: Spinner = findViewById(R.id.spinner3_secondConversion)
-
-        ArrayAdapter.createFromResource(
-            this,
-            R.array.currencies,
-            android.R.layout.simple_spinner_item
-        ).also { adapter ->
-            // Specify the layout to use when the list of choices appears
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            // Apply the adapter to the spinner
-            spinner1.adapter = adapter
-        }
-
-        ArrayAdapter.createFromResource(
-            this,
-            R.array.currencies2,
-            android.R.layout.simple_spinner_item
-        ).also { adapter ->
-            // Specify the layout to use when the list of choices appears
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            // Apply the adapter to the spinner
-            spinner2.adapter = adapter
-        }
-        ArrayAdapter.createFromResource(
-            this,
-            R.array.distance,
-            android.R.layout.simple_spinner_item
-        ).also { adapter ->
-            // Specify the layout to use when the list of choices appears
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            // Apply the adapter to the spinner
-            spinner3.adapter = adapter
-        }
-        ArrayAdapter.createFromResource(
-            this,
-            R.array.weight,
-            android.R.layout.simple_spinner_item
-        ).also { adapter ->
-            // Specify the layout to use when the list of choices appears
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            // Apply the adapter to the spinner
-            spinner4.adapter = adapter
-        }
 
         spinner1.onItemSelectedListener = (object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
-                baseCurrency = parent?.getItemAtPosition(pos).toString()
-                getApiResult()
+                convertFrom = spinner1.selectedItem.toString()
+                conversion()
             }
 
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-                TODO("Not yet implemented")
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                convertFrom = spinner2.selectedItem.toString()
             }
         })
         spinner2.onItemSelectedListener = (object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
-                convertedToCurrency = parent?.getItemAtPosition(pos).toString()
-                getApiResult()
+                convertTo = spinner2.selectedItem.toString()
+                conversion()
             }
 
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-                TODO("Not yet implemented")
-            }
-        })
-        spinner3.onItemSelectedListener = (object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
-                convertedToDistance = parent?.getItemAtPosition(pos).toString()
-                getDistanceResult()
-            }
-
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-                TODO("Not yet implemented")
-            }
-        })
-        spinner4.onItemSelectedListener = (object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
-                convertedToWeight = parent?.getItemAtPosition(pos).toString()
-                getWeightResult()
-            }
-
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-                TODO("Not yet implemented")
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                convertTo = spinner2.selectedItem.toString()
             }
         })
     }
